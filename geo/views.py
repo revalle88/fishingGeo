a5039f6f74db46bc10a0000f92773046c6b412ab
revalle88 <revalle88@gmail.com>
2018-07-04 14:49:51 +0300

    pounds on map

--- geo/views.py
+++ geo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
+from django.http import HttpResponse
+from .models import Pound
+
+
+def index(request):
+	pounds = Pound.objects.all()
+	return render(request, 'index.html',  {"pounds": pounds})
+
+ #   return HttpResponse("Hello, world. You're at the polls index.")
+
+def pounds(request):
+	pounds = Pound.objects.all()
+	
+	#pounds_for_js = json.dumps(pounds)
+	return render(request, 'pounds.html',  {"pounds": pounds})
